989c5ba86c2fc055ff0e683cee316dadc52d6e31
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2022-07-18 22:40:03 +0300

    Updated aeindus plugin code

--- plugins/aeindus-custom/plugin-main.cpp
+++ plugins/aeindus-custom/plugin-main.cpp
@@ -1,20 +1,4 @@
-/*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program. If not, see <https://www.gnu.org/licenses/>
-*/
+// clang-format off
 
 #include <Windows.h>
 #include <sstream>
@@ -22,9 +6,9 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <cstring>
 #include <thread>
 #include <obs-module.h>
+#include "plugin-macros.generated.hpp"
 #include "UI\obs-frontend-api\obs-frontend-api.h"
 #include "util.hpp"
-#include "plugin-macros.generated.hpp"
 
 #define STOP MessageBoxA(NULL, "Test", "Test", MB_OK)
 
@@ -32,15 +16,13 @@ OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
 void custom_callback(obs_frontend_event event, void* private_data) {
-	if (event == OBS_FRONTEND_EVENT_SCENE_CHANGED) {
-		blog(LOG_INFO, "SCENE_CHANGED");
-
+	if (event== OBS_FRONTEND_EVENT_SCENE_CHANGED) {
 		std::string current_scene_name = obs_frontend_get_current_scene_name();
 
-		OutputDebugStringA("Current scene name:");
-		OutputDebugStringA(current_scene_name.c_str());
+		blog(LOG_INFO, "SCENE_CHANGED");
+		blog(LOG_INFO, "Current scene name: %s",current_scene_name.c_str());
 
-		if (current_scene_name.find("PScena") != std::string::npos) {
+		if (current_scene_name.find("PScena")!= std::string::npos) {
 			// Pause the other app projector
 			ToggleJWLibraryProjector(PROJ_OPERATION::MINIMIZE);
 			
