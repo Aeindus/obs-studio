583e55eeaef35c6e485d4f76cc49dd98781f0fb4
Reznicencu Sergiu <25268629+AntonVonDelta@users.noreply.github.com>
2022-07-20 02:01:27 +0300

    Added VScena keyword; Swapped vcam image with year text

--- plugins/aeindus-custom/plugin-main.cpp
+++ plugins/aeindus-custom/plugin-main.cpp
@@ -25,6 +25,13 @@ void custom_callback(obs_frontend_event event, void* private_data) {
 		blog(LOG_INFO, "SCENE_CHANGED");
 		blog(LOG_INFO, "Current scene name: %s",current_scene_name.c_str());
 
+		// Activate the other app projector
+		// The default behaviour is to leave this projector on
+		// even when this scene is switched out.
+		if (current_scene_name.find("JWScena") != std::string::npos) {
+			ToggleJWLibraryProjector(PROJ_OPERATION::MAXIMIZE);
+		}
+
 		if (current_scene_name.find("PScena")!= std::string::npos) {
 			// Pause the other app projector
 			ToggleJWLibraryProjector(PROJ_OPERATION::MINIMIZE);
@@ -41,9 +48,11 @@ void custom_callback(obs_frontend_event event, void* private_data) {
 			obs_frontend_close_projectors(EXTERNAL_MONITOR);
 		}
 
-		// Activate the other app projector
-		if (current_scene_name.find("JWScena") != std::string::npos) {
-			ToggleJWLibraryProjector(PROJ_OPERATION::MAXIMIZE);
+		if (current_scene_name.find("VScena") != std::string::npos) {
+			obs_frontend_stop_virtualcam();
+		} else {
+			if(!obs_frontend_virtualcam_active())
+				obs_frontend_start_virtualcam();
 		}
 
 	} else if (event == OBS_FRONTEND_EVENT_SCENE_LIST_CHANGED) {
