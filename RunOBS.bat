3baa0023091a96b4f42675bacaa18c664534473f
AntonVonDelta <25268629+AntonVonDelta@users.noreply.github.com>
2022-09-09 01:17:09 +0300

    Improved RunObs script

--- RunOBS.bat
+++ RunOBS.bat
@@ -7,26 +7,39 @@ set obs_data_path=%appdata%\obs-studio\basic
 
 if "%1"=="/?" goto help
 if "%1"=="/backup" goto create_backup
+if "%1"=="\backup" goto create_backup
+if not "%1"=="" goto help
 goto restore_and_run
 
 :help
 	echo.
+	echo =============== Help ===============
 	echo 	[]		Restores the backup and starts the program
 	echo 	[/backup] 	Replaces/Creates a backup
+	echo.
 	exit
 :create_backup
+	echo.
+	echo =============== Backup profiles ===============
 	rmdir /S /Q %backup_path% > nul
 	if exist "%backup_path%" (
 		echo Backup delete failed! Most likely the files in backup folder are in use.
 	)
 	xcopy "%obs_data_path%\*.*" "%backup_path%\"  /E /Y /R /I
+	
+	echo.
+	echo Backed up profiles:
 	for %%i in ("%backup_path%\scenes\*.json") do (
-		echo Backed up profile %%i
+		echo           %%~nxi
 	)
-	timeout 2
+	
+	echo.
+	pause
 	exit
 	
 :restore_and_run
+	echo.
+	echo =============== Restore and Run ===============
 	call :restore
 	call :run
 	exit /b 0
@@ -38,9 +51,14 @@ goto restore_and_run
 		exit /b 
 	)
 	xcopy "%backup_path%\*.*" "%obs_data_path%\" /E /Y /R /I
+	
+	echo.
+	echo Restored profiles:
 	for %%i in ("%backup_path%\scenes\*.json") do (
-		echo Restored profile %%i
+		echo           %%~nxi
 	)
+	
+	echo.
 	timeout 2
 	exit /b
 	
